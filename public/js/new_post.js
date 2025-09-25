9011ee2866423dda776b257c7c3127d97e44a768
Papp Patrik <ppatrik15@mailbox.unideb.hu>
2025-09-25 18:28:04 +0200

    feat: add function for new comment button

---
+++ public/js/new_post.js
@@ -0,0 +1,25 @@
+async function createPost(e) {
+	e.preventDefault();
+	const content = document.getElementById('post-msg-box');
+	var length = 'long';
+	if (content.value.length <= 250) {
+		length = 'short';
+	} else if (content.value.length <= 1800) {
+		length = 'medium';
+	} else {
+		length = 'long';
+	}
+
+	const res = await fetch('/api/new_post', {
+		method: 'post',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ content: content.value, length: length }),
+	});
+
+	if (res.ok) console.log('New post added!');
+	else {
+		const errorData = await res.json();
+
+		alert(errorData.message);
+	}
+}
